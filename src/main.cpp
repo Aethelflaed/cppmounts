5d6a30d9731ca7f575c509e4053dec07e5be41ea
Geoffroy PLANQUART <geoffroy@planquart.fr>
2012-04-27 16:23:55 +0200

    Added tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
+#include "cppmounts.hpp"
+#include <iostream>
 
 int main(int argc, const char** argv)
 {
+	std::vector<filesystem::mount> mounts = filesystem::mount::all();
+
+	for (size_t i = 0; i < mounts.size(); i++)
+	{
+		std::cout <<
+			mounts[i].getName() << "\t" <<
+			mounts[i].getPath() << "\t" <<
+			mounts[i].getType() << "\t" <<
+			std::endl;
+	}
 	return 0;
 }
 
